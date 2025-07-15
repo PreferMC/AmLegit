1943b524f0a52030c3b902999a0913fffb2c19f0
commandf1 <mkhjmy@qq.com>
2025-07-15 08:37:40 +0800

    Changes

--- src/main/java/space/commandf1/amlegit/check/impl/packet/badpacket/BadPacketsB.java
+++ src/main/java/space/commandf1/amlegit/check/impl/packet/badpacket/BadPacketsB.java
@@ -1,18 +1,18 @@
 package space.commandf1.amlegit.check.impl.packet.badpacket;
 
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
-import com.github.retrooper.packetevents.event.PacketSendEvent;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import org.bukkit.plugin.Plugin;
 import space.commandf1.amlegit.check.defaults.*;
 import space.commandf1.amlegit.config.check.CheckConfigHandler;
 import space.commandf1.amlegit.tracker.impl.PositionTracker;
+import space.commandf1.amlegit.util.PlayerUtil;
 
 public class BadPacketsB extends Check implements Setbackable {
 
     @CheckConfigHandler(name = "tolerance")
     @AlertDescription(name = "Tolerance")
-    private long tolerance = 100;
+    private long tolerance = 5;
 
     public BadPacketsB(Plugin plugin) {
         super("BadPackets", 3, "Checks for C0F disabler", "B", plugin);
@@ -23,17 +23,17 @@ public class BadPacketsB extends Check implements Setbackable {
 
     @Override
     public void onCheck(CheckHandler handler) {
+        int ping = PlayerUtil.getPing(handler.getPlayerData().getPlayer());
+
         if (handler.getEvent() instanceof PacketReceiveEvent event) {
             if (event.getPacketType() == PacketType.Play.Client.WINDOW_CONFIRMATION) {
-                handler.decreaseBuffer(handler.buffer());
+                handler.decreaseBuffer(2);
             } else {
                 handler.increaseBuffer(1);
             }
-        } else if (handler.getEvent() instanceof PacketSendEvent) {
-            handler.increaseBuffer(1);
         }
 
-        if (handler.buffer() >= this.tolerance) {
+        if (handler.buffer() > this.tolerance + ((double) (ping * 2L) / 1000) / 0.05D) {
             handler.fail();
         }
     }
