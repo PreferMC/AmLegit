a69dc5c93f7abce6e19c7103c9d447b920865102
commandf1 <mkhjmy@qq.com>
2025-08-15 18:09:42 +0800

    delete some shits

--- src/main/java/space/commandf1/amlegit/AmLegitPlugin.java
+++ src/main/java/space/commandf1/amlegit/AmLegitPlugin.java
@@ -9,7 +9,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import space.commandf1.amlegit.check.CheckManager;
-import space.commandf1.amlegit.check.impl.move.analysis.AnalysisA;
 import space.commandf1.amlegit.check.impl.move.groundspoof.GroundSpoofA;
 import space.commandf1.amlegit.check.impl.move.keepsprint.KeepSprintA;
 import space.commandf1.amlegit.check.impl.move.scaffold.ScaffoldA;
@@ -17,14 +16,12 @@ import space.commandf1.amlegit.check.impl.packet.badpacket.BadPacketsA;
 import space.commandf1.amlegit.check.impl.packet.badpacket.BadPacketsB;
 import space.commandf1.amlegit.check.impl.packet.badpacket.BadPacketsC;
 import space.commandf1.amlegit.command.AmLegitCommand;
-import space.commandf1.amlegit.config.ai.AIDataConfig;
 import space.commandf1.amlegit.config.check.CheckConfig;
 import space.commandf1.amlegit.config.settings.SettingsConfig;
 import space.commandf1.amlegit.listener.CheckListener;
 import space.commandf1.amlegit.listener.DebugListener;
 import space.commandf1.amlegit.listener.PlayerListener;
 
-import java.io.File;
 import java.util.Arrays;
 
 public class AmLegitPlugin extends JavaPlugin {
@@ -56,19 +53,11 @@ public class AmLegitPlugin extends JavaPlugin {
                 new BadPacketsC(this),
                 new KeepSprintA(this),
                 new GroundSpoofA(this),
-                new ScaffoldA(this),
-                new AnalysisA(this)
+                new ScaffoldA(this)
         );
 
         this.checkConfig = new CheckConfig(this, this.checkManager);
         this.settingsConfig = new SettingsConfig(this);
-
-        File[] aiData = new File(this.getDataFolder(), "ai-data").listFiles();
-        if (aiData != null) {
-            for (File aiDatum : aiData) {
-                AIDataConfig.of(this, aiDatum);
-            }
-        }
 
         this.registerBukkitListeners(new PlayerListener(this));
         PacketEvents.getAPI().getEventManager().registerListener(new CheckListener(this),
